560ca1aa1888ffce7353cf5ae2798f2042275b8a
theankushrai <iamankushrai@gmail.com>
2024-09-30 10:13:53 +0530

    multiple people and notification

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 from bs4 import BeautifulSoup
 import emoji
 import re
+import plyer
+from plyer import notification
 
 file=open('snapchat.html',"r",encoding='utf-8')
 soup = BeautifulSoup(file,'html.parser')
 
 text=soup.get_text()
-index=text.find('New Chat')
-text=text[:index]
-text=emoji.replace_emoji(text,' ')
-text=text.split(' ')[2:]
 
-name=' '.join(text)
+#cleaning data
+text=emoji.replace_emoji(text,' ') #replacing all the emojis
+text=re.split("New Chat",text)
+text=text[:-1] #removing last li because its not required
 
-print(name,"sent a new message")
+message=""
+for it in text:
+    it=it.split(' ')[-2:] #name of a person
+    name=' '.join(it) #join the name back
+    message+=name+" sent a new message !!\n"
+    
+ 
+notification.notify( #notification from plyer
+            title="New Message",
+            message=message,
+            app_name="Message Reminder",
+            timeout=10  # Duration the notification stays on the screen (in seconds)
+        )
+    
+
+
+
--- setup.sh
+++ setup.sh
@@ -4,3 +4,8 @@
 echo "Installing Beautiful Soup..."
 pip3 install beautifulsoup4
 echo "Beautiful Soup installation complete!"
+
+#install plyer
+echo "Installing Plyer"
+pip3 install plyer
+echo "Plyer installed"
